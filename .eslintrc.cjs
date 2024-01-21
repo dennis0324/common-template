bb615be3f8207e5ea1de11ad043277b8d29cdd5e
dennis ko <dennis0324@naver.com>
2024-01-21 19:54:43 +0900

    add: readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    // project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    // sourceType: 'module',
   },
   env: {
     node: true,
---
+++ readme.md
@@ -0,0 +1,5 @@
+# This is template of ESM 2021
+
+this is not perfect
+
+this is for my template only, you can use if you want to
